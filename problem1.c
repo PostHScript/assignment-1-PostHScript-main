e8d8ac411b90b299a5be27128b2b01889c7ddbe7
PostHScript <paulhsantana@gmail.com>
2023-09-06 23:16:04 -0500

    first commit

---
+++ problem1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int input = 0;
+
+    scanf("%f\n", input);
+
+    if (input > 5 and input < 15) {
+        printf("Case 1\n");
+    } else if (input = 20) {
+        printf("Case 2\n");
+    } else {
+        printf("Case 3\n");
+    }
+
+    return 0;
+}
